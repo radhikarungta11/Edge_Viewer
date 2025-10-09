772b5226a0f1e9c6be68246adf5bb1c98c7867e1
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2025-10-09 19:53:49 +0530

    Update image_processor.h

--- jni/image_processor.h
+++ jni/image_processor.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL
+Java_com_example_edgeviewer_NativeBridge_processYToRgba(
+    JNIEnv* env, jclass clazz,
+    jobject yPlane, jint width, jint height, jint rowStride,
+    jboolean doCanny, jobject outRgba);
+
+#ifdef __cplusplus
+}
+#endif
 

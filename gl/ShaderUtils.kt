2da04d4f38bdb59e002a414eeebbc02a8547e825
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2025-10-09 19:36:14 +0530

    Update ShaderUtils.kt

--- gl/ShaderUtils.kt
+++ gl/ShaderUtils.kt
@@ -1,1 +1,23 @@
 
+package gl
+import android.opengl.GLES20
+
+object ShaderUtils {
+    fun load(src: String, type: Int): Int {
+        val id = GLES20.glCreateShader(type)
+        GLES20.glShaderSource(id, src)
+        GLES20.glCompileShader(id)
+        val ok = IntArray(1)
+        GLES20.glGetShaderiv(id, GLES20.GL_COMPILE_STATUS, ok, 0)
+        if (ok[0] == 0) throw RuntimeException("Shader: " + GLES20.glGetShaderInfoLog(id))
+        return id
+    }
+    fun link(v: Int, f: Int): Int {
+        val p = GLES20.glCreateProgram()
+        GLES20.glAttachShader(p, v); GLES20.glAttachShader(p, f); GLES20.glLinkProgram(p)
+        val ok = IntArray(1)
+        GLES20.glGetProgramiv(p, GLES20.GL_LINK_STATUS, ok, 0)
+        if (ok[0] == 0) throw RuntimeException("Program: " + GLES20.glGetProgramInfoLog(p))
+        return p
+    }
+}
